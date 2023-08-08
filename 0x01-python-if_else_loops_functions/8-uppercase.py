a7b4afad79dfc63e1fd51d33c2f48013565380c8
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-08-08 21:28:17 +0300

    v0.110

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+
+def uppercase(str):
+    cpy = ""
+    for i in range(len(str)):
+        if ord(str[i]) in range(97, 123):
+            cpy += chr(ord(str[i]) - 32)
+        else:
+            cpy += str[i]
+    print(cpy)
+uppercase("Best")
