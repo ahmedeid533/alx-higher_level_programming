699d24ce9051d79f2d1f1ff51287c1f1c4ac6699
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-08-17 02:15:34 +0300

    v0.15C5

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string == None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
     dicM = {'MMM': 3000, 'MM': 2000,  'M': 1000}
-    dicC = {'CM': 900, 'DCCC': 800, 'DCC': 700, 'DC': 600, 'CD': 400, 'D': 500, 'CCC': 300,
-            'CC': 200,  'C': 100, }
+    dicC = {'CM': 900, 'DCCC': 800, 'DCC': 700, 'DC': 600, 'CD': 400, 
+            'D': 500, 'CCC': 300, 'CC': 200,  'C': 100, }
     dicX = {'XC': 90, 'LXXX': 80, 'LXX': 70, 'LX': 60,
             'XL': 40, 'L': 50, 'XXX': 30, 'XX': 20,  'X': 10}
     dicI = {'IX': 9, 'VIII': 8, 'VII': 7, 'VI': 6, 'IV': 4, 'V': 5,
