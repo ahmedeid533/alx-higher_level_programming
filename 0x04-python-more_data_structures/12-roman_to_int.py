b620c1de72cc0e97bb53ac23b6004eba716405d9
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-08-17 01:57:59 +0300

    v0.15C3

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or not roman_string or roman_string == None:
+    if not isinstance(roman_string, str) or roman_string == None:
         return 0
     dicM = {'MMM': 3000, 'MM': 2000, 'CM': 900, 'M': 1000}
-    dicC = {'DCCC': 800, 'DCC': 700, 'DC': 600,  'CD': 400, 'D': 500, 'CCC': 300,
+    dicC = {'DCCC': 800, 'DCC': 700, 'DC': 600, 'CD': 400, 'D': 500, 'CCC': 300,
             'CC': 200, 'XC': 90, 'C': 100, }
     dicX = {'LXXX': 80, 'LXX': 70, 'LX': 60,
             'XL': 40, 'L': 50, 'XXX': 30, 'XX': 20, 'IX': 9, 'X': 10}
-    dicI = {'VIII': 8, 'VII': 7,  'VI': 6, 'IV': 4, 'V': 5,
+    dicI = {'VIII': 8, 'VII': 7, 'VI': 6, 'IV': 4, 'V': 5,
             'III': 3, 'II': 2, 'I': 1}
     result = 0
     i = 0
@@ -15,17 +15,20 @@ def roman_to_int(roman_string):
         if k in roman_string[i:]:
             i += len(k)
             result += v
-            break
+            if v != 900:
+                break
     for k, v in dicC.items():
         if k in roman_string[i:]:
             i += len(k)
             result += v
-            break
+            if v != 90:
+                break
     for k, v in dicX.items():
         if k in roman_string[i:]:
             i += len(k)
             result += v
-            break
+            if v != 9:
+                break
     for k, v in dicI.items():
         if k in roman_string[i:]:
             i += len(k)
