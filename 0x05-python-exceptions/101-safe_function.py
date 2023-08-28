2df649cf7feb224763821aa1bab8879642d33d55
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-08-28 19:29:31 +0300

    v0.01e

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as exp:
+        import sys
+        print("Exception: {}".format(exp), file=sys.stderr)
+        return None
