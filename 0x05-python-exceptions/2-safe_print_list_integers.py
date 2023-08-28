ad9a387db8dbad90d12b80ffec0527bef6355487
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-08-28 18:34:55 +0300

    v0.00f

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    j = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            j += 1
+        except not IndexError:
+            continue
+    print("")
+    return (j)
