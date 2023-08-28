1b451aa34decf04bf600eb58bac91331ff5488c0
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-08-28 17:36:18 +0300

    p0.006

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,8 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i],end="")
             j+=1 
-        
+        print("")
+        return j
     except:
-        o = 0
-    print("")
-    return j
+        print("")
+        return j
