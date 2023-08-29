bfe425528f90f22f2bf0c99faf61ca81e3a8449e
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-08-29 23:38:47 +0300

    p0.04A

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""test test"""
+
+
+class Square:
+    """test test"""
+
+    __size = 0
+
+    @property
+    def size(self):
+        """why this"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """why this"""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __init__(self, size=0):
+        """why this"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """calculate area"""
+        
+        return (self.__size ** 2)
+
+    def __repr__(self):
+        """size"""
+
+        return self.__size
