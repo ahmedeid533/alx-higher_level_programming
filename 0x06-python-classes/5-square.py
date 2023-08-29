55f5234dc32de9e5fd9849876b4ef9027945f282
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-08-29 20:48:26 +0300

    v0.033

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -39,7 +39,10 @@ class Square:
     def my_print(self):
         """print square of #"""
 
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print("#", end="")
+        if self.__size == 0:
             print("")
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print("")
