5486236e8ce2af58c78688ac5ec1ce78b78ab537
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-08-29 23:01:53 +0300

    v0.03F

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,14 +8,7 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         """why this"""
 
-        if (not isinstance(position, tuple)
-                or not isinstance(position[0], int)
-                or not isinstance(position[1], int)
-                or position[0] < 0
-                or position[1] < 0
-                or len(position) != 2):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif not isinstance(size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
