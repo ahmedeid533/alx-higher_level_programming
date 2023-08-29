734c7d9e1393fe311b535c9f041961f6462160f8
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-08-29 20:25:41 +0300

    v0.02d

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""test test"""
+
+
+class Square:
+    """test test"""
+
+    def __init__(self, size=0):
+        """why this"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
