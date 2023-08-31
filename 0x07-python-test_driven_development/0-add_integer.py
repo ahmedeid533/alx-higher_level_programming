bd5c7134d4bb8c7ad68ba2aecc1516c6628ce8eb
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-08-31 18:06:21 +0300

    p0.04C

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""test example"""
+
+def add_integer(a, b=98):
+    """add intgers"""
+    if (not isinstance(a, (int, float))):
+        raise TypeError("a must be an integer")
+    elif (not isinstance(b, (int, float))):
+        raise TypeError("b must be an integer")
+    a_ = int(a)
+    b_ = int(b)
+    return a_ + b_
