27814ece5c7173572d980645f4b0d59d29ed3c1f
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-09-02 22:40:05 +0300

    p0.059

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -10,7 +10,7 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
     if size == 0:
-        print("")
+        return
     else:
         for i in range(size):
             for j in range(size):
