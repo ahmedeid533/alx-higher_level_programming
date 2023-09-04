b9d6d418fc7b0b61d4a91e0844a850c42bcc07bc
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-09-05 02:53:52 +0300

    p0.082

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -86,7 +86,6 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
-
     @classmethod
     def square(cls, size=0):
         """Return a new Rectangle with width and height equal to size.
