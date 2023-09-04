6e61bf20cd1308291ebec5b2c01e4c2f57296894
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-09-05 01:22:57 +0300

    p0.069

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""react angle class"""
+
+
+class Rectangle:
+    """rectangel class"""
+
+    __height = 0
+    __width = 0
+
+    @property
+    def width(self):
+        """get W"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set W"""
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """get H"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set H"""
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    def area(self):
+        """calculate area"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """calculate perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (2 * (self.__width + self.__height))
+
+    def __str__(self):
+        """"print rect of #"""
+
+        temp = ""
+        if self.perimeter == 0:
+            temp = ""
+        else:
+            for i in range(self.__height):
+                for j in range(self.__width):
+                    temp += "#"
+                if i < self.__height - 1:
+                    temp += "\n"
+        return str(temp)
