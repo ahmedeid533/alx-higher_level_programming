073267f1ca6168ccfc3fdcd5782c47f1418fc489
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-09-11 22:39:14 +0300

    P0x0A.00A

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""is subbclass"""
+
+
+def is_kind_of_class(obj, a_class):
+    """is subbclass"""
+    if type(obj) == a_class:
+        return True
+    return issubclass(obj, a_class)
