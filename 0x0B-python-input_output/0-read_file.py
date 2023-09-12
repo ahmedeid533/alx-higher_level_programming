f6e3255079a0d5311f982e8798f60c9a2c0d6ad6
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-09-12 19:06:46 +0300

    P0x0B.003

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ def read_file(filename=""):
     read_data = ""
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
-    print(read_data, end="") 
+    print(read_data, end="")
