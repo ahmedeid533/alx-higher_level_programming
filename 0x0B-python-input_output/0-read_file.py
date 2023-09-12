e3a326a2bfcb4b702e99a5bb8aa73d6672452475
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-09-12 19:02:44 +0300

    P0x0B.0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""read files"""
+
+
+def read_file(filename=""):
+    """read files from filename .txt"""
+    read_data = ""
+    with open('filename', encoding="utf-8") as f:
+        read_data = f.read()
+    print(read_data) 
