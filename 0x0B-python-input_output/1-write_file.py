fc59b49afd692a1467504b2a0e0385401a78537c
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-09-12 23:35:02 +0300

    P0x0B.004

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""read files"""
+
+
+def write_file(filename="", text=""):
+    """read files from filename .txt"""
+    with open(filename, encoding="utf-8") as f:
+        return f.write(text)
