06892e5f9ef56548460949d1a99b97aece89b818
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-09-13 00:46:26 +0300

    P0x0B.01C

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,12 +7,12 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    tri = [[1]]
-    while len(tri) != n:
-        tri = tri[-1]
+    TRI = [[1]]
+    while len(TRI) != n:
+        tri = TRI[-1]
         temp = [1]
         for i in range(len(tri) - 1):
             temp.append(tri[i] + tri[i + 1])
         temp.append(1)
-        tri.append(temp)
-    return tri
+        TRI.append(temp)
+    return TRI
