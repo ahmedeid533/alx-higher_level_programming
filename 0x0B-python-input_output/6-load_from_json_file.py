7dfe2448a65c3a2484d5af2155734655ffcc720f
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-09-13 00:04:37 +0300

    P0x0B.00D

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""load_from_json_file"""
+
+
+def load_from_json_file(filename):
+    """read files from filename .txt"""
+    import json
+    with open(filename, 'w') as f:
+        return json.load(f)
