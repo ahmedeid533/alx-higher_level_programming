6e5e5f9ea8448140a7b67e49c454a440ee6e9237
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-11-13 20:58:46 +0200

    0x12 vC.02

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const numbers = process.argv.slice(2).map(Number);
-  const sorted = numbers.sort((a, b) => a - b);
-  console.log(sorted[sorted.length - 1]);
+  const sorted = numbers.sort(function (a, b) { return b - a; })
+  console.log(sorted[1]);
 }
