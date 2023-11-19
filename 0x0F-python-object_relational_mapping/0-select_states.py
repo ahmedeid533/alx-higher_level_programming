7b2c5f05951064ce6d2d07a81930176e60d33e23
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-11-19 07:47:55 +0200

    .\0-select_states.py v0.07

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
 	conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
 						 passwd=sys.argv[2], db=sys.argv[3])
 	cur = conn.cursor()
-	cur.execute("SELECT * FROM states") # HERE I have to know SQL to grab all states in my database
+	cur.execute("SELECT * FROM states")
 	query_rows = cur.fetchall()
-	for state in query_rows: # HERE: no SQL query, only objects!
+	for state in query_rows:
 		print(state)
 	cur.close()
 	conn.close()
