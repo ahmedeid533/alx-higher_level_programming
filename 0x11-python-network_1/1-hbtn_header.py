db82ce5fe03af59016a896a4964789afce4327ef
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-12-03 21:50:27 +0200

    Python 0x11 V0.02

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""get the header"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    with urllib.request.urlopen(sys.argv[1]) as data:
+        parse = response.headers.get('X-Request-Id')
+        print(parse)
