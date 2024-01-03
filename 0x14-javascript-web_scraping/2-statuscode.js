70e866c56e3b66375d7a3b23036e572fe19dcf05
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-01-03 16:50:39 +0200

    web scrap v0.04

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,8 +2,8 @@
 const request = require('request');
 request(process.argv[2], function (error, response) {
   if (error) {
-	console.log(error);
+    console.log(error);
   } else {
-	console.log('code:', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
